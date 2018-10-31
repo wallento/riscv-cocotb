275b85186a9a26fb347542d61d589432a9d1df1e
Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>
2018-10-31 17:59:12 +0100

    picorv32: Verify code execution

---
+++ riscvcocotb/cores/picorv32.py
@@ -0,0 +1,38 @@
+import cocotb
+from cocotb.triggers import Timer, RisingEdge, FallingEdge
+from riscvcocotb.testbench import Testbench
+
+from riscvmodel.variant import RV32I
+from riscvmodel.golden import GoldenProgramEndException
+from riscvmodel.isa import InstructionNOP, InstructionAND
+
+class PicoRV32Testbench(Testbench):
+    _clk = "clk"
+    _reset = "resetn"
+    _reset_polarity = False
+
+    def __init__(self, dut):
+        super().__init__(RV32I, dut, self.rvfi_lookup(dut))
+        dut.mem_ready <= 0
+        dut.irq <= 0
+        self.end_of_program = False
+
+    @cocotb.coroutine
+    def busif(self):
+        while True:
+            if int(self.dut.mem_valid) == 1:
+                if int(self.dut.mem_instr) == 1:
+                    if self.end_of_program:
+                        insn = InstructionNOP().encode()
+                    else:
+                        try:
+                            insn = self.fetch(int(self.dut.mem_addr))
+                        except GoldenProgramEndException:
+                            insn = InstructionNOP().encode()
+                            self.end_of_program = True
+                    self.dut.mem_rdata <= insn
+                    self.dut.mem_ready <= 1
+                    yield FallingEdge(self.sig_clk)
+                    self.dut.mem_ready <= 0
+            else:
+                yield FallingEdge(self.sig_clk)
--- riscvcocotb/testbench.py
+++ riscvcocotb/testbench.py
@@ -3,18 +3,22 @@ from collections import deque
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge, FallingEdge
+from cocotb.result import TestFailure, TestSuccess
 
-from riscvmodel.model import Model
+from riscvmodel.golden import GoldenUnbuffered, GoldenProgramEndException, traces_from_rvfi
 from riscvmodel.program.tests import ADDITest
+from riscvmodel.types import RVFISignals
+from riscvmodel.code import decode
+
 
 class Testbench(object):
-    def __init__(self, variant, dut):
+    def __init__(self, variant, dut, rvfi):
         self.dut = dut
         self.sig_clk = self.dut.__getattr__(self._clk)
         self.sig_reset = self.dut.__getattr__(self._reset)
+        self.rvfi = rvfi
         cocotb.fork(Clock(self.sig_clk, 2).start())
-
-        self.model = Model(variant)
+        self.model = GoldenUnbuffered(variant)
 
     @cocotb.coroutine
     def reset(self, duration = 10):
@@ -29,16 +33,45 @@ class Testbench(object):
         else:
             self.sig_reset <= 1
         cocotb.fork(self.busif())
+        cocotb.fork(self.rvfi_monitor())
 
     @cocotb.coroutine
     def basic(self):
-        self.insns = deque(ADDITest().insns)
-        for i in range(10):
+        self.model.load_program(ADDITest())
+        self.model.reset()
+        for i in range(20):
             yield RisingEdge(self.sig_clk)
 
+    @cocotb.coroutine
+    def rvfi_monitor(self):
+        while True:
+            if self.rvfi.valid.value == 1:
+                try:
+                    rvfi = self.rvfi_extract_values(self.rvfi)
+                    self.model.commit(traces_from_rvfi(rvfi), insn = decode(rvfi.insn))
+                except ValueError:
+                    raise TestFailure(str(ValueError()))
+                except GoldenProgramEndException:
+                    raise TestSuccess()
+                self.dut._log.info("Comitted")
+            yield RisingEdge(self.sig_clk)
+
+    @staticmethod
+    def rvfi_lookup(dut) -> RVFISignals:
+        signals = {}
+        for v in RVFISignals._fields:
+            signals[v] = dut.__getattr__("rvfi_{}".format(v))
+        return RVFISignals()._replace(**signals)
+
+    def rvfi_extract_values(self, signals: RVFISignals) -> RVFISignals:
+        values = {}
+        for v in RVFISignals._fields:
+            values[v] = signals.__getattribute__(v).__int__()
+        return RVFISignals()._replace(**values)
+
+
     def fetch(self, pc: int):
-        insn = self.insns.popleft()
-        self.model.execute(insn)
+        insn = self.model.fetch(pc)
         return insn.encode()
 
     def lb(self, addr: int):
